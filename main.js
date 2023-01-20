ae816cdc1f153f8467f425bdeb7507e66ee3b13a
ijimenez08 <isaac.jmimenez@utp.ac.pa>
2023-01-20 12:28:47 -0500

    modified:   CSS/hoja.css 	modified:   CSS/styles.css 	modified:   ganado.html 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ function rejistro()
     let litro= prompt("cuantos ligros");
     
 }
+/*function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }*/
